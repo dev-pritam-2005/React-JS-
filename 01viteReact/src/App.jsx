89dfc537b3843449b0f6b6e91caca854526e7b9d
dev-pritam-2005 <pritamdutta35689@gmail.com>
2024-03-11 20:42:10 +0530

    new react changes

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1> chai aur pritam & React with vite | Pritam Dutta | in 2nd video of react js  we  know how react file exicute ..i use vite & react  for  creating this project.   </h1>
+  )
+}
+
+export default App
